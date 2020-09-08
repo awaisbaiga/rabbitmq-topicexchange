952244feac5ef1cdee8a675266b62ba6c4739190
Awais Baiga <70507684+awaisbaiga@users.noreply.github.com>
2020-09-08 12:36:58 +0500

    create topic exchange for multiple queues

---
+++ src/main/java/com/example/demo/MulQueuesTopicExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulQueuesTopicExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulQueuesTopicExchangeApplication.class, args);
+	}
+
+}
